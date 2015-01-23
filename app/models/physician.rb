acb701ecf707bf0d99af1ae2fe2a4072f27b96cf
flada-auxv <aseknock@gmail.com>
2015-01-23 12:58:45 +0900

    Fix accepts_nested_attributes_for target

--- app/models/physician.rb
+++ app/models/physician.rb
@@ -2,5 +2,5 @@ class Physician < ActiveRecord::Base
   has_many :appointments
   has_many :patients, through: :appointments
 
-  accepts_nested_attributes_for :patients
+  accepts_nested_attributes_for :appointments
 end

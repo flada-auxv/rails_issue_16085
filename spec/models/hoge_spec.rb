a5916714526ee71b7996f22c666df6a0f7ab4763
flada-auxv <aseknock@gmail.com>
2015-01-23 13:03:16 +0900

    Add result

--- spec/models/hoge_spec.rb
+++ spec/models/hoge_spec.rb
@@ -11,5 +11,7 @@ describe '' do
       physician.patients = [Patient.first]
       physician.save!
     }.to_not raise_error
+    # 4.1.1 => pass
+    # 4.1.2 => expected no Exception, got #<ActiveRecord::StatementInvalid: Mysql2::Error: Field 'physician_id' doesn't have a default value: INSERT INTO `appointments` (`created_at`, `patient_id`, `updated_at`) VALUES ('2015-01-23 04:01:18', 12, '2015-01-23 04:01:18')> with backtrace:
   }
 end

f2ee74e230c061feb2cee27f864998b9d1360c56
flada-auxv <aseknock@gmail.com>
2015-01-23 12:15:39 +0900

    Add rspec

--- Gemfile
+++ Gemfile
@@ -9,3 +9,5 @@ gem 'coffee-rails', '~> 4.0.0'
 gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
+
+gem 'rspec-rails'
--- test/models/patients_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class PatientsTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
